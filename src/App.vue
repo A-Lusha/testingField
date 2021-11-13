4d890106c29172714d278d1ad16761453557b39b
Alex <alex@webweave.io>
2021-11-13 12:56:17 -0600

    scss inclusion

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <h1>{{ messageContent }}</h1>
+    <router-link to="/">Home</router-link>
   </div>
   <router-view/>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      messageContent: 'testing',
+    };
+  },
+};
+</script>
+
 <style lang="scss">
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $primary-color;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: $primary-color;
-
-    &.router-link-exact-active {
-      color: $primary-color;
-    }
-  }
-}
 </style>
