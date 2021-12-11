7b322846bb49b2d5c3a3c958cd2f0614c4f29eba
Alex Nelson <alexander.j.lusha@gmail.com>
2021-12-10 21:19:43 -0600

    Added KaaS (Kanye as a Service) to the home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Home.vue</h1>
-  <p class="">ommodi amet alias quasi saepe ducimus, fugiat eaque magni quod quae unde mollit
+  <p v-if="quote">{{ quote }}</p>
+  <p v-else>ommodi amet alias quasi saepe ducimus, fugiat eaque magni quod quae unde mollit
     ia ex debitis aliquid velit. Voluptas eligendi ne
     cessitatibus commodi cumque vero ullam, non accusamus maxime fugiat.
     ommodi amet alias quasi saepe ducimus, fugiat eaque magni quod quae unde mollit
@@ -24,8 +25,29 @@
 </template>
 
 <script>
+import { axios } from "@/common/api.service.js";
+
 export default {
   name: 'Home',
-
+  data() {
+    return {
+      quote: null,
+    }
+  },
+  methods: {
+    async getKanye() {
+      let endpoint = 'https://api.kanye.rest/';
+      try {
+        const response = await axios.get(endpoint);
+        this.quote = response.data.quote;
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+  },
+  created() {
+    document.title = "KanyeTime";
+    this.getKanye();
+  },
 };
 </script>
